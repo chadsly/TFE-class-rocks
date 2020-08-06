262b6b7ad412a6f0156d277057444bb2d0e4437d
Chad <chad@expansiagroup.com>
2020-08-06 15:15:04 -0400

    switch to guardian

--- main.tf
+++ main.tf
@@ -12,11 +12,15 @@ module "high_altitude" {
     aws = aws.gov-west
   }
 
+  app_download_url          = "https://nexus.di2e.net/nexus3/repository/Private_OADCGS-GEOINT/Server/Guardian/1.5.17.1342/Guardian-1.5.17.1342.zip"
   app_tpl_filepath_filename = "templates/app.tpl"
+  app_version               = "Guardian-1.5.17.1342"
   application_name          = "gitlab-for-class"
   aws_ami                   = "ami-e7063586"
   ec2_size                  = "t2.medium"
   http_proxy                = "http://internal-transit-c-rELBProx-1FIWKJ1YWJ83C-1424253041.us-gov-west-1.elb.amazonaws.com:3128/"
+  NEXUS_PASSWORD            = "${var.high_altitude_NEXUS_PASSWORD}"
+  NEXUS_USERNAME            = "${var.high_altitude_NEXUS_USERNAME}"
   no_proxy                  = "'localhost,http://127.0.0.1,10.0.0.0/8'"
   private_subnets           = ["subnet-9d3d7deb", "subnet-2c3b7b48", "subnet-5468cc0d"]
   public_subnets            = ["subnet-3b37774d", "subnet-2f3e7e4b", "subnet-e27dd9bb"]
